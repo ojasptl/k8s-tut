22aa04fccb4563b4de208bc320f25cff0fc9f4f8
Ojas Patil <161685651+ojasptl@users.noreply.github.com>
2025-10-06 13:28:16 +0000

    Solved Merge Conflict in nginx/persistent_volume.sh

--- nginx/persistent_volume.sh
+++ nginx/persistent_volume.sh
@@ -4,8 +4,4 @@ kubectl apply -f PersistentVolume.yaml
 kubectl get pv
 
 # To create a Persistent Volume Claim
-<<<<<<< HEAD
 kubectl apply -f persistent-volume-claim.yaml
-=======
-kubectl apply -f persistent-volume-claim.yaml
->>>>>>> 2d362a6b76c89238d7cd9e7ebfbb26572b74502a
