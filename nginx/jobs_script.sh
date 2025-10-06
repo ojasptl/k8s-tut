66ded9d85f0ef2392c67c655d8fb67759940d7df
Ojas Patil <161685651+ojasptl@users.noreply.github.com>
2025-10-06 13:07:27 +0000

    Add Kubernetes resources for backup and job management, including CronJob, Job, PersistentVolume, and PersistentVolumeClaim

---
+++ nginx/jobs_script.sh
@@ -0,0 +1,3 @@
+kubectl logs pod/demo-job-5xkcx -n nginx
+# Get logs of a specific pod in the nginx namespace
+kubectl get cronjob -n nginx
---
+++ nginx/persistent_volume.sh
@@ -0,0 +1,6 @@
+# To create a Persistent Volume
+kubectl apply -f PersistentVolume.yaml
+# To check for the persistent volume
+kubectl get pv
+
+# To create a Persistent Volume Claim
